4b42faa4e9ad9c01cf23965d7e9b1db0ef58dcf9
Ankit4Dec <ankit27866@gmail.com>
2025-10-19 00:05:17 +0530

    Adding First Commit to this Repo

---
+++ FirstClass.java
@@ -0,0 +1,30 @@
+// class FirstClass{
+//     public static void main(String args[]){{
+//         System.out.println("Hello World");
+//     }}
+// // 
+
+
+// class FirstClass{
+//     public static void main(String args[]){
+//         System.out.println("*");
+//         System.out.println("**");
+//         System.out.println("***");
+//         System.out.println("****");
+//     }
+// }
+
+
+class FirstClass{
+    public static void main(String args[]){
+        int a = 25;
+        int b = 10;
+        int sum = a + b;
+        // System.out.print(sum);
+        int diff = a-b;
+        // System.out.println(diff);
+        int mul = a * b;
+        System.out.println(mul);
+        
+    }
+}
