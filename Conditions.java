6cdb20a9675f2d16bfb71953004beda6eb5c67c7
Ankit4Dec <ankit27866@gmail.com>
2025-10-20 00:56:00 +0530

    Else if

--- Conditions.java
+++ Conditions.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Conditions {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
         // if(age>18){
         //     System.out.println("Adult");
@@ -11,11 +12,19 @@ public class Conditions {
         //     System.out.println("Not Adult");
         // }
 
-        if(x%2==0){
-            System.out.println("Even");
-        }else{
-            System.out.println("Odd");
-        }
+        // if(x%2==0){
+        //     System.out.println("Even");
+        // }else{
+        //     System.out.println("Odd");
+        // }
+
+        if(a==b){
+            System.out.println("Equal");
+        }else if(a>b){
+                System.out.println("a is greater");
+            } else{
+                System.out.println("b is greater");
+            }
 
     }
 }
