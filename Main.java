683a5ceb153646b8f3f0f61f9eb2a8d9233df25a
Ankit4Dec <ankit27866@gmail.com>
2025-10-20 00:06:59 +0530

    System In and System Out Concept

--- Main.java
+++ Main.java
@@ -2,13 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        //Input
-        Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        //nextInt()
-        //nextFloat()
-        System.out.println(name);
-        sc.close();
+    //     //Input
+    //     Scanner sc = new Scanner(System.in);
+    //     String name = sc.nextLine();
+    //     // Read user input as a string and print it
+    //     System.out.println(name);
+    //     sc.close();
+    // }
+
+    // Scanner sc = new Scanner(System.in);
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // int sum = a+b;
+    // System.out.println(sum);
+    // sc.close();
+
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int mul = a*b;
+    System.out.println(mul);
+    sc.close();
+
+
     }
     
 }
