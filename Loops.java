0df4b2d0b1d985e2dba7d8216d5aecc27a004d92
Ankit4Dec <ankit27866@gmail.com>
2025-10-23 00:58:38 +0530

    Loops in Java

---
+++ Loops.java
@@ -0,0 +1,9 @@
+public class Loops {
+
+    public static void main(String agrs[]){
+        for(int counter=0;counter<3;counter++){
+            System.out.println("Holla There "+ counter);
+        }
+    }
+
+}
