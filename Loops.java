d0be2c13cd08c9b06700295f3012587cd0253aea
Ankit4Dec <ankit27866@gmail.com>
2025-10-23 01:14:12 +0530

    Trying While loop

--- Loops.java
+++ Loops.java
@@ -1,8 +1,21 @@
 public class Loops {
 
     public static void main(String agrs[]){
-        for(int counter=0;counter<3;counter++){
-            System.out.println("Holla There "+ counter);
+        // for(int counter=0;counter<3;counter++){
+        //     System.out.println("Holla There "+ counter);
+        // }
+
+        //another try for FOR loop
+        // for(int counter = 0; counter<11;counter++){
+        //     System.out.println(counter+" ");
+        // }
+
+        // try for while loop
+
+        int i=0;
+        while (i<17) {
+            System.out.println(i);
+            i++;
         }
     }
 
