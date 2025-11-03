d26f83c0b216059cdde714a1ef32c61dfc847f99
Ankit4Dec <ankit27866@gmail.com>
2025-11-03 23:13:00 +0530

    Factorial Number

--- Functions.java
+++ Functions.java
@@ -14,22 +14,42 @@ public class Functions {
     // }
 
 
-    public static int mulForTwoNum(int a, int b){
-        int product = a * b;
-        return product;
+    // public static int mulForTwoNum(int a, int b){
+    //     int product = a * b;
+    //     return product;
+    // }
+
+    public static void printFactorial(int n){
+        //loop
+        if(n<0){
+            System.out.println("Invalid Number");
+            return;
+        }
+
+        int factorial =1;
+
+        for(int i =n; i>=1; i--){
+            factorial= factorial *i;
+        }
+
+        System.out.println(factorial);
+        return;
     }
 
     public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int n = sc.nextInt();
+
+        printFactorial(n);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
 
         // int sum = calculateSum(a, b);
         // System.out.println(sum);
        
-        int product = mulForTwoNum(a, b);
-        System.out.println("Multiply for Two Number is => "+ product);
+        // int product = mulForTwoNum(a, b);
+        // System.out.println("Multiply for Two Number is => "+ product);
 
     }
     
