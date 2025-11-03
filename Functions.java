2263ab9c805001183cd41626fd0edfc624ba9a25
Ankit4Dec <ankit27866@gmail.com>
2025-11-03 22:54:27 +0530

    Function Example

--- Functions.java
+++ Functions.java
@@ -2,17 +2,26 @@ import java.util.Scanner;
 
 public class Functions {
 
-    public static void printMyName(String name){
-        System.out.println(name);
-        return;
+    // public static void printMyName(String name){
+    //     System.out.println(name);
+    //     return;
+    // }
+
+
+    public static int calculateSum(int a, int b){
+        int sum = a + b;
+        return sum;
     }
 
     public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        printMyName(name); // Calling Function 
+        int sum = calculateSum(a, b);
+        System.out.println(sum);
+        
 
     }
     
