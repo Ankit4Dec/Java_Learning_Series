8ed7ebc23adf687d67f64f3aabe9f5625490c2cb
Ankit4Dec <ankit27866@gmail.com>
2025-11-23 18:41:05 +0530

    Number patter using Scanner, Which is help to take input from the User

--- PattOne.java
+++ PattOne.java
@@ -1,4 +1,11 @@
+import java.util.*;
+
 public class PattOne {
+
+
+
+    // Method 1: Using Fixed Integers
+
     // public static void main(String args[]){
     //     for(int i= 1;i<=4;i++){
     //         for(int j=1;j<=4;j++){
@@ -8,10 +15,28 @@ public class PattOne {
     //     }
     // }
 
+    // Method 2: Using Variable
+
+
+    // public static void main(String args[]){
+    //     int n = 5;
+    //     int m = 4;
+    //     for(int i= 1;i<=n;i++){
+    //         for(int j=1;j<=m;j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println("");
+    //     }
+    // }
+
+
+
+    // Method 1: Taking input from the user for the Pattern Size
 
     public static void main(String args[]){
-        int n = 5;
-        int m = 4;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
         for(int i= 1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 System.out.print("*");
