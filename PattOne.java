6fa177f5a862b5c8dc1b2117ef96cd49c82c8349
Ankit4Dec <ankit27866@gmail.com>
2025-11-23 18:33:56 +0530

    Using Variable for the Pattern

--- PattOne.java
+++ PattOne.java
@@ -1,7 +1,19 @@
 public class PattOne {
+    // public static void main(String args[]){
+    //     for(int i= 1;i<=4;i++){
+    //         for(int j=1;j<=4;j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println("");
+    //     }
+    // }
+
+
     public static void main(String args[]){
-        for(int i= 1;i<=4;i++){
-            for(int j=1;j<=4;j++){
+        int n = 5;
+        int m = 4;
+        for(int i= 1;i<=n;i++){
+            for(int j=1;j<=m;j++){
                 System.out.print("*");
             }
             System.out.println("");
