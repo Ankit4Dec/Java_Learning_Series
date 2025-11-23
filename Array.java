433edd8c3fd96134a1b552acf53da3d2ba554024
Ankit4Dec <ankit27866@gmail.com>
2025-11-23 17:51:26 +0530

    Arrays Basics

---
+++ Array.java
@@ -0,0 +1,13 @@
+public class Array {
+
+    public static void main(String args[]){
+        int marks[] = new int[3];
+        marks[0] = 75; //Phy
+        marks[1] = 95; //Maths
+        marks[2] = 62; //Chem
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+    
+}
