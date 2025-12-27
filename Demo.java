2b83b823187cfc70ef3792716ec3d1d7a01a1563
Ankit4Dec <ankit27866@gmail.com>
2025-12-27 22:13:18 +0530

    Added a Class in the Demo file and the class name is Calculater and after complie, it creates a new file

--- Demo.java
+++ Demo.java
@@ -1,12 +1,27 @@
+class Calculator{
+
+    // int a;
+
+    public int add(int n1, int n2){
+        int r = n1 + n2;
+        return r;
+    }
+}
+
+
 public class Demo {
     
     public static void main(String a[]){
-        int num1 = 4;
-        int num2 = 5;
+        int num1 = 14;
+        int num2 = 588;
 
-        int result = num1 + num2;
+        // int result = num1 + num2;
+        // System.out.println(result);
 
-        System.out.println(result);
+        Calculator cal = new Calculator();
+        int result = cal.add(num1, num2);
+        System.err.println(result);
+
     }
 
 
