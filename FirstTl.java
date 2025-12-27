e671008ce5d34cac9cf6292e47ae123aa6ea1688
Ankit4Dec <ankit27866@gmail.com>
2025-12-27 21:50:08 +0530

    Commit all in once

---
+++ FirstTl.java
@@ -0,0 +1,12 @@
+public class FirstTl{
+    public static void main(String args[]){
+        // byte a = 125;
+        // int a = 257;
+        // byte k = (byte) a;
+
+
+        float f = 5.6f;
+        int m = (int) f;
+        System.out.println(m);
+    }
+}
