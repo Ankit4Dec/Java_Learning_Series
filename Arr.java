829b81644b4260fad3e3982bd75efa28dbabceec
Ankit4Dec <ankit27866@gmail.com>
2025-12-31 18:59:39 +0530

    Array

---
+++ Arr.java
@@ -0,0 +1,16 @@
+public class Arr {
+    
+    public static void main(String a[]){
+        int nums[] = new int[4];
+        nums[1] = 6;
+        nums[2] = 1;
+        nums[3] = 8;
+        nums[0] = 4;
+        // int num[] = {5,7,8};
+        for(int i=0; i<nums.length ;i++){
+
+            System.out.println(nums[i]);
+        }
+    }
+
+}
